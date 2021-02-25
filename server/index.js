5a4e1afff0ed7c80844db7f5636c534fa57eb343
Recep <recbay@gmail.com>
2021-02-25 17:52:37 +0100

    2e

--- server/index.js
+++ server/index.js
@@ -42,8 +42,8 @@ const urunSema = {
   },
   marka: String,
   aciklama: String,
-  ind_fiyat: String,
-  normal_fiyat: String,
+  ind_fiyat: Number,
+  normal_fiyat: Number,
   yildiz: {
     sayi: Number,
     puan: String,
@@ -69,11 +69,18 @@ const yorumSema = {
   dislike: Number,
 };
 
+const kategoriSema = {
+  kategori_isim: String,
+  kategori_url: String,
+  kategori_aciklama: String,
+};
+
 ///////// MONGODB ŞEMALARI - BİTİŞ
 
 ///////// MONGODB MODELLERİ - BAŞLANGIÇ
 const Urun = mongoose.model("Urun", urunSema);
 const Yorum = mongoose.model("Yorum", yorumSema);
+const Kategori = mongoose.model("Kategori", kategoriSema);
 ///////// MONGODB MODELLERİ - BİTİŞ
 
 app.get("/", function (req, res) {
@@ -83,13 +90,13 @@ app.get("/", function (req, res) {
 //////////////////////////                  URUN              /////////////////////////////
 app.post("/api/urun/olusturma", function (req, res) {
   var urun = new Urun({
-    isim: "Bu bir gömlektir",
+    isim: "Süvari Gömlek",
     kategori: "Gömlek",
     kategori_url: "gomlek",
     resimler: {
-      bir: "/images/items/1.jpg",
-      iki: "/images/items/2.jpg",
-      uc: "/images/items/3.jpg",
+      bir: "/images/items/3.jpg",
+      iki: "/images/items/1.jpg",
+      uc: "/images/items/1.jpg",
       dort: "/images/items/4.jpg",
     },
     stok: {
@@ -98,11 +105,11 @@ app.post("/api/urun/olusturma", function (req, res) {
       l: 9,
       xl: 9,
     },
-    marka: "Polo",
+    marka: "Süvari",
     aciklama:
       "Virgil Abloh’s Off-White is a streetwear-inspired collection that continues to break away from the conventions of mainstream fashion. Made in Italy, these black and brown Odsy-1000 low-top sneakers.",
-    ind_fiyat: "29.99",
-    normal_fiyat: "39.99",
+    ind_fiyat: 29.99,
+    normal_fiyat: 39.99,
     yildiz: {
       sayi: 20,
       puan: "4.4",
@@ -168,6 +175,125 @@ app.get("/api/urun/benzerurunler/:kategori_url/:urunid", function (req, res) {
   )
     .sort({ satilma: -1 })
     .limit(4);
+});
+
+/////////////////////////            KATEGORİ SAYFASI           /////////////////////////////
+app.get("/api/kategori/:kategori_url/:bulundugu_sayfa", function (req, res) {
+  var sayfa = req.params.bulundugu_sayfa;
+  var secim = req.query.secim;
+  var markalar =
+    req.query.marka === null || req.query.marka === "" ? "" : req.query.marka;
+
+  var minFiyat = parseInt(req.query.min);
+  var maxFiyat = parseInt(req.query.max);
+
+  var aramaKriterleri = {
+    kategori_url: req.params.kategori_url,
+  };
+
+  if (markalar !== "") {
+    var bosArray = markalar.split(",");
+    if (bosArray.length > 0) {
+      aramaKriterleri["marka"] = {
+        $in: bosArray,
+      };
+    }
+  }
+
+  if (minFiyat > 0 && maxFiyat === 0) {
+    maxFiyat = 99999999999;
+  }
+
+  if ((minFiyat === 0 && maxFiyat > 0) || (minFiyat > 0 && maxFiyat > 0)) {
+    aramaKriterleri["ind_fiyat"] = {
+      $gt: minFiyat,
+      $lt: maxFiyat,
+    };
+  }
+
+  var kriter = {};
+
+  if (secim === "1") {
+    kriter = {
+      _id: -1,
+    };
+  } else if (secim === "2") {
+    kriter = {
+      satilma: -1,
+    };
+  } else if (secim === "3") {
+    kriter = {
+      ind_fiyat: 1,
+    };
+  }
+
+  Urun.find(aramaKriterleri, function (err, gelenVeri) {
+    if (!err) {
+      res.send(gelenVeri);
+    } else {
+      res.send([
+        {
+          sonuc: "hata",
+        },
+      ]);
+    }
+  })
+    .sort(kriter)
+    .limit(6)
+    .skip((sayfa - 1) * 6);
+});
+
+app.get("/api/urunsayisi/:kategori_url", function (req, res) {
+  var markalar =
+    req.query.marka === null || req.query.marka === "" ? "" : req.query.marka;
+
+  var minFiyat = parseInt(req.query.min);
+  var maxFiyat = parseInt(req.query.max);
+
+  var aramaKriterleri = {
+    kategori_url: req.params.kategori_url,
+  };
+
+  if (markalar !== "") {
+    var bosArray = markalar.split(",");
+    if (bosArray.length > 0) {
+      aramaKriterleri["marka"] = {
+        $in: bosArray,
+      };
+    }
+  }
+
+  if (minFiyat > 0 && maxFiyat === 0) {
+    maxFiyat = 99999999999;
+  }
+
+  if ((minFiyat === 0 && maxFiyat > 0) || (minFiyat > 0 && maxFiyat > 0)) {
+    aramaKriterleri["ind_fiyat"] = {
+      $gt: minFiyat,
+      $lt: maxFiyat,
+    };
+  }
+
+  Urun.find(aramaKriterleri, function (err, gelenVeri) {
+    if (!err) {
+      res.send({
+        toplam: gelenVeri.length,
+      });
+    } else {
+      res.send([
+        {
+          sonuc: "hata",
+        },
+      ]);
+    }
+  });
+});
+
+app.get("/api/kategori/filtre/marka/:kategori_url", function (req, res) {
+  Urun.distinct("marka", function (err, gelenVeri) {
+    if (!err) res.send(gelenVeri);
+    else res.send(err);
+  });
 });
 
 /////////////////////////             POPÜLER ÜRÜNLER           //////////////////////////////
@@ -242,6 +368,57 @@ app.post("/api/yorum", function (req, res) {
       //// daha sonra..
     } else {
       res.send({ sonuc: "hata" });
+    }
+  });
+});
+
+/////////////////////////////   KATEGORİ API     ///////////////
+
+app.post("/api/kategori_bireysel/olustur", function (req, res) {
+  var kategori = new Kategori({
+    kategori_isim: req.body.isim,
+    kategori_url: req.body.url,
+    kategori_aciklama: req.body.aciklama,
+  });
+
+  kategori.save(function (err) {
+    if (!err) {
+      res.send({
+        sonuc: "başarılı",
+      });
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.get("/api/kategori_bireysel/:kategori_url", function (req, res) {
+  Kategori.find(
+    { kategori_url: req.params.kategori_url },
+    function (err, gelenVeri) {
+      if (!err) {
+        res.send(gelenVeri);
+      } else {
+        res.send([
+          {
+            sonuc: "hata",
+          },
+        ]);
+      }
+    }
+  );
+});
+
+app.get("/api/kategori_liste", function (req, res) {
+  Kategori.find({}, function (err, gelenVeri) {
+    if (!err) {
+      res.send(gelenVeri);
+    } else {
+      res.send([
+        {
+          sonuc: "hata",
+        },
+      ]);
     }
   });
 });
